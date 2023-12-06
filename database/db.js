2cb013b8b2c214328860551d83aadf65366ef1f4
Mike <eaaamigk@students.eaaa.dk>
2023-12-06 12:35:30 +0100

    Bump

--- database/db.js
+++ database/db.js
@@ -1,10 +1,9 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-let connString =
-	'postgresql://postgres:Storepatter123!!@db.rfaqykrfrnyqupvubahm.supabase.co:5432/postgres';
+let connString = process.env.connString;
 
 const db = new Sequelize(connString, {
-	logging: false,
+  logging: false,
 });
 
 module.exports = db;
